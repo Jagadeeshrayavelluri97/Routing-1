af05e28afe91d331ff45892a8fda60864746927b
Jagadeeeshrayavelluri97 <rayavellurijagadeesh@gmail.com>
2024-09-05 10:21:16 +0000

    solution

---
+++ src/components/About/index.js
@@ -0,0 +1,19 @@
+// Write your JS code here
+
+import './index.css'
+
+const About = () => (
+  <div className="about-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      alt=""
+      className="about-img"
+    />
+    <h1 className="about-heading">About</h1>
+    <p className="about-paragraph">
+      I Love to create! I am frontend web developer
+    </p>
+  </div>
+)
+
+export default About
